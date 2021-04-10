7929ee8039e40d2c9839c290bea366a14333086e
Vishnu N V <56473993+carbonxx@users.noreply.github.com>
2021-04-10 16:07:55 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,63 +1,71 @@
-var fgImage = null;
-var bgImage = null;
-var fgCanvas;
-var bgCanvas;
+ar imgBg, imgFg, canvasBg, canvasFg, ctxFg, ctxBg;
 
-function loadForegroundImage() {
-  var file = document.getElementById("fgfile");
-  fgImage = new SimpleImage(file);
-  fgCanvas = document.getElementById("fgcan");
-  fgImage.drawTo(fgCanvas);
+function uploadBg(){
+  canvasBg = document.getElementById("canBg");
+  canvasBg.width = 500;
+  canvasBg.height = 300;
+  var fileInput = document.getElementById("bginput");
+  //var filename = fileInput.value;
+  imgBg = new SimpleImage(fileInput);  
+  imgBg.drawTo(canvasBg);
 }
 
-function loadBackgroundImage() {
-  var file = document.getElementById("bgfile");
-  bgImage = new SimpleImage(file);
-  bgCanvas = document.getElementById("bgcan");
-  bgImage.drawTo(bgCanvas);
+function uploadFg(){
+  canvasFg = document.getElementById("canFg");
+  canvasFg.width = 500;
+  canvasFg.height = 300;
+  var fileInput2 = document.getElementById("fginput");  
+  imgFg = new SimpleImage(fileInput2);
+  imgFg.drawTo(canvasFg);
 }
 
-function createComposite() {
-  // this function creates a new image with the dimensions of the foreground image and returns the composite green screen image
-  var output = new SimpleImage(fgImage.getWidth(),fgImage.getHeight());
-  var greenThreshold = 240;
-  for (var pixel of fgImage.values()) {
+function clearCanvas(){
+  ctxFg = canvasFg.getContext("2d");
+  ctxBg = canvasBg.getContext("2d");  
+  ctxFg.clearRect(0,0,canvasFg.width,canvasFg.height);
+  ctxBg.clearRect(0,0,canvasBg.width,canvasBg.height);  
+}
+
+function clearAll(){
+  clearCanvas();
+  var fileInput = document.getElementById("bginput");
+  var fileInput2 = document.getElementById("fginput");
+  fileInput.value = null;
+  fileInput2.value = null;
+}
+
+
+function makeComposite(){  
+  if(imgFg == null || !imgFg.complete()){
+    alert("Foreground Not Loaded.");
+    return;
+  }
+  if(imgBg == null || !imgBg.complete()){
+    alert("Background Not Loaded.");
+    return;
+  }
+  clearCanvas();   
+  var slider = document.getElementById("slider");
+  var greenThreshold = slider.value;
+  var imgOutput = new SimpleImage(imgFg.getWidth(),imgFg.getHeight());  
+  for(var pixel of imgFg.values()){
     var x = pixel.getX();
     var y = pixel.getY();
-    if (pixel.getGreen() > greenThreshold) {
-      //pixel is green, use background
-      var bgPixel = bgImage.getPixel(x,y);
-      output.setPixel(x,y,bgPixel);
+    if(pixel.getGreen() > greenThreshold){
+      var bgPixel = imgBg.getPixel(x, y);
+      imgOutput.setPixel(x, y, bgPixel);
+    }else{
+      imgOutput.setPixel(x, y, pixel);
     }
-    else {
-      //pixel is not green, use foreground
-      output.setPixel(x,y,pixel);
-    }
-  }
-  return output;
+  }  
+  imgOutput.drawTo(canvasFg);
 }
 
-function doGreenScreen() {
-  //check that images are loaded
-  if (fgImage == null  || ! fgImage.complete()) {
-    alert("Foreground image not loaded");
-  }
-  if (bgImage == null || ! bgImage.complete()) {
-    alert("Background image not loaded");
-  }
-  // clear canvases
-  clearCanvas();
-  // call createComposite, which does green screen algorithm and returns a composite image
-  var finalImage = createComposite();
-  finalImage.drawTo(fgCanvas);
-}
 
-function clearCanvas() {
-  doClear(fgCanvas);
-  doClear(bgCanvas);
-}
-
-function doClear(canvas) {
-  var context = canvas.getContext("2d");
-  context.clearRect(0,0,canvas.width,canvas.height);
+function downloadImg(){
+  var img = canvasFg.toDataURL("image/png",1.0).replace("image/png","image/octect-stream");
+  var link = document.createElement("a");
+  link.download = "composite.png";
+  link.href = img;
+  link.click();
 }
